99d09a0f3c4ed4fbdff037fdda90ffbaceec7da3
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-05-12 15:42:59 -0700

    redirect

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import AboutPage from "./AboutPage";
 import HomePage from "./HomePage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import PageNotFound from "./PageNotFound";
 
 function App() {
@@ -15,6 +20,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/courses" component={CoursesPage} />
           <Route exact path="/about" component={AboutPage} />
+          <Redirect from="/about-page" to="/about" />
           <Route component={PageNotFound} />
         </Switch>
       </div>
