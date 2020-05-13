2bdb011d0dc45615fbbe917fbdec55e9cef723ac
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-05-13 14:17:15 -0700

    prompt on page transition

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,29 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, Prompt } from "react-router-dom";
 
 function CourseList(props) {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author ID</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.courses.map((course) => (
-          <tr key={course.id}>
-            <td>
-              <Link to={`/course/${course.title}`}>{course.title}</Link>
-            </td>
-            <td>{course.authorId}</td>
-            <td>{course.category}</td>
+    <React.Fragment>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.courses.map((course) => (
+            <tr key={course.id}>
+              <td>
+                <Link to={`/course/${course.title}`}>{course.title}</Link>
+              </td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Prompt when={true} message="Are you sure to leave this page?" />
+    </React.Fragment>
   );
 }
 
